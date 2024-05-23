f08df4eb80e8ca8512dd3cf0e93229f45ffac50a
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-24 01:08:33 +0500

    complete ExploreMenu ui

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Home.css"
 import Header from "../../components/Header/Header"
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
   return (
     <div>
 
       <Header />
+      <ExploreMenu />
       {/* http://www.dodear.com/en/videos/383387 */}
     </div>
   )
