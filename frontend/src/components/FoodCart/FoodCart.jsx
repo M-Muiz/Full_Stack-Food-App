f797159ccb2e1d62579f817fbc30fc9e456f564a
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-24 19:53:50 +0500

    complete food cart

--- frontend/src/components/FoodCart/FoodCart.jsx
+++ frontend/src/components/FoodCart/FoodCart.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodCart.css'
 import { assets } from '../../assets/assets'
 
 const FoodCart = ({ id, name, price, image, description }) => {
+
+    const [itemCount, setItemCount] = useState(0);
+
     return (
         <div className='food-cart'>
             <div className="food-cart-image-container">
                 <img className='food-cart-image' src={image} alt="food-image" />
+                {
+                    !itemCount ? <img className='add' onClick={()=> setItemCount(prev => prev + 1)} src={assets.add_icon_white} alt="add-icon" /> :
+                     <div className="food-cart-counter">
+                        <img onClick={()=> setItemCount(prev => prev - 1)} src={assets.remove_icon_red} alt="remove-icon" />
+                        <p>{itemCount}</p>
+                        <img onClick={()=> setItemCount(prev => prev + 1)} src={assets.add_icon_green} alt="" />
+                     </div>
+                }
             </div>
             <div className="food-cart-info">
                 <div className="food-cart-name-rating">
