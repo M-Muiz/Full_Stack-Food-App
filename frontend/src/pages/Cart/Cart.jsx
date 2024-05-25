5f5905519245875d87e26e22e616e09209fff048
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-25 13:46:43 +0500

    working on cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./Cart.css"
-
+import { StoreContext } from '../../context/StoreContext'
 const Cart = () => {
+
+  const {cartItem, addToCart, removeFromCrt } = useContext(StoreContext);
+
   return (
     <div>
       cart
