729f2fc3cdfb7d1c65bd8feaf2eaed524b44d808
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-26 14:45:12 +0500

    create multer connection

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,12 @@
+import foodModel from "../models/foodModel.js";
+import fs from "fs";
+
+
+const addFood = async (req, res) => {
+    // const { name, description, image, price, category } = req.body;
+    // if (!name || !description || !image || !price || !category) {
+
+};
+
+
+export {addFood}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 
 
@@ -11,7 +12,11 @@ const port = 4000;
 app.use(express.json());
 app.use(cors());
 
-connectDB()
+connectDB();
+
+
+app.use("/api/food", foodRouter)
+
 
 app.get("/", (req, res) => {
     res.send("Hello from server");
