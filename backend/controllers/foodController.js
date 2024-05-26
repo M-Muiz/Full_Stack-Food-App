43fb6afb9ca5043e11e3abf1ea6e29dadfe177c2
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-26 14:51:57 +0500

    create addfood add point

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -3,10 +3,25 @@ import fs from "fs";
 
 
 const addFood = async (req, res) => {
-    // const { name, description, image, price, category } = req.body;
-    // if (!name || !description || !image || !price || !category) {
+
+    let image_filename = `${req.file.filename}`;
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        image: image_filename,
+    });
+
+    try {
+        await food.save();
+        res.json({ success: true, message: "Food added successfully" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Failed to add food" });
+    }
 
 };
 
 
-export {addFood}
+export { addFood }
