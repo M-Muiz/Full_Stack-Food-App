05b3d9d21f0694346b40ab17a1adb83b4c58d017
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-26 15:45:39 +0500

    create remove food endpoint

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -35,4 +35,18 @@ const listFood = async (req, res) => {
 };
 
 
-export { addFood, listFood }
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () => []);
+
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Food removed successfully" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Failed to remove food" });
+    }
+};
+
+
+export { addFood, listFood, removeFood };
