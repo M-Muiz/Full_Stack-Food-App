37eadd79bdc0ab45bbf8b0491e4ad56225273545
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-27 23:30:07 +0500

    create removefromcart controller

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,10 +2,8 @@ import useModel from "../models/userModel.js"
 
 const addToCart = async (req, res) => {
     try {
-        let userData = await useModel.find({ _id: req.body.userId });
-        console.log(userData)
-        let cartData = await userData[0].cartData;
-        console.log(cartData)
+        let userData = await useModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
         if (!cartData[req.body.itemId]) {
             cartData[req.body.itemId] = 1;
         } else {
@@ -21,7 +19,18 @@ const addToCart = async (req, res) => {
 };
 
 const removeFromCart = async (req, res) => {
-
+    try {
+        let userData = await useModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
+        if (cartData[req.body.itemId] > 0) {
+            cartData[req.body.itemId] -= 1;
+        }
+        await useModel.findByIdAndUpdate(req.body.userId, { cartData });
+        res.json({ success: true, message: "Item removed from cart" })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Failed to remove item from cart" })
+    }
 };
 
 const getCartItems = async (req, res) => {
