b514e47387c674e1474b9c1307c1be6b461d3e43
m-muiz <mmuizsiddiqui@gmail.com>
2024-06-25 14:34:26 +0500

    little changes

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-    res.send("Hello from server");
+    res.json("Hello from server");
 });
 
 
